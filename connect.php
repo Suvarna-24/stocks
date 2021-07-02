d7faa96b90bec47909d92c45e2355414c6a15e01
Suvarna-24 <47240328+Suvarna-24@users.noreply.github.com>
2021-07-02 17:29:18 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,9 +3,10 @@
 $servername = "sql6.freesqldatabase.com";
 $username = "sql6422177";
 $password = "dKNFHXkf6Z";
+$db="sql6422177";
 
 // Create connection
-$con = new mysqli($servername, $username, $password);
+$con = new mysqli($servername, $username, $password ,$db);
 
 // Check connection
 if ($con->connect_error) {
